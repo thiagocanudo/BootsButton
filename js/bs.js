f07688ce47c8d40cf6239d86d915c8029f1d5f7f
Thiago Canudo <thiago@thiagocanudo.com.br>
2017-02-15 01:20:35 -0200

    Atualização BS buttons v3.3.7

--- js/bs.js
+++ js/bs.js
@@ -13,97 +13,97 @@ $('document').ready(function(){
     }
     $(':text').bind("keyup",msg);
     // set tooltips icon class
-    $('ul.the-icons li i').attr('data-toggle','tooltip');
-    $('ul.the-icons li i').attr('data-original-title','tooltip');
-    $('ul.the-icons li i').mouseover(function(){
+    $('ul.the-icons li span').attr('data-toggle','tooltip');
+    $('ul.the-icons li span').attr('data-original-title','tooltip');
+    $('ul.the-icons li span').mouseover(function(){
         var $txtToolTip = $(this).attr('class');
         $(this).attr('data-original-title',$txtToolTip);
     });
     $('#code-icon-left').hide();
 
-    //btn-none
-    $('a#btn-none').click(function(){
+    //btn-default
+    $('a#btn-default').click(function(){
         $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-        $('#bt').removeClass("btn-primary btn-info btn-success btn-warning btn-danger btn-inverse btn-link");
-        $('#code-btn-color').text('');
+        $('#bt').addClass('btn-default').removeClass("btn-primary btn-info btn-success btn-warning btn-danger btn-inverse btn-link");
+        $('#code-btn-color').text(' btn-default');
     });
 
     //btn-primary
     $('a#btn-primary').click(function(){
            $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-           $('#bt').addClass('btn-primary').removeClass("btn-none btn-info btn-success btn-warning btn-danger btn-inverse btn-link");   
+           $('#bt').addClass('btn-primary').removeClass("btn-default btn-info btn-success btn-warning btn-danger btn-inverse btn-link");   
            $('#code-btn-color').text(' btn-primary');
     });
 
     //btn-info
     $('a#btn-info').click(function(){
            $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-           $('#bt').addClass('btn-info').removeClass("btn-none btn-primary btn-success btn-warning btn-danger btn-inverse btn-link");   
+           $('#bt').addClass('btn-info').removeClass("btn-default btn-primary btn-success btn-warning btn-danger btn-inverse btn-link");   
            $('#code-btn-color').text(' btn-info');  
     });
 
     //btn-success
     $('a#btn-success').click(function(){
             $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-            $('#bt').addClass('btn-success').removeClass("btn-none btn-primary btn-info btn-warning btn-danger btn-inverse btn-link");   
+            $('#bt').addClass('btn-success').removeClass("btn-default btn-primary btn-info btn-warning btn-danger btn-inverse btn-link");   
             $('#code-btn-color').text(' btn-success');
     });
 
     //btn-warning
     $('a#btn-warning').click(function(){
             $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-            $('#bt').addClass('btn-warning').removeClass("btn-none btn-primary btn-info btn-success btn-danger btn-inverse btn-link");   
+            $('#bt').addClass('btn-warning').removeClass("btn-default btn-primary btn-info btn-success btn-danger btn-inverse btn-link");   
             $('#code-btn-color').text(' btn-warning');
     });
 
     //btn-danger
     $('a#btn-danger').click(function(){
             $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-            $('#bt').addClass('btn-danger').removeClass("btn-none btn-primary btn-info btn-success btn-warning btn-inverse btn-link");   
+            $('#bt').addClass('btn-danger').removeClass("btn-default btn-primary btn-info btn-success btn-warning btn-inverse btn-link");   
             $('#code-btn-color').text(' btn-danger');
     });
 
     //btn-inverse
     $('a#btn-inverse').click(function(){
             $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-            $('#bt').addClass('btn-inverse').removeClass("btn-none btn-primary btn-info btn-success btn-warning btn-danger btn-link");   
+            $('#bt').addClass('btn-inverse').removeClass("btn-default btn-primary btn-info btn-success btn-warning btn-danger btn-link");   
             $('#code-btn-color').text(' btn-inverse');
     });
 
     //btn-link
     $('a#btn-link').click(function(){
             $(this).addClass('active').parent().siblings('p').find('a').removeClass('active');
-            $('#bt').addClass('btn-link').removeClass("btn-none btn-primary btn-info btn-success btn-warning btn-danger btn-inverse");   
+            $('#bt').addClass('btn-link').removeClass("btn-default btn-primary btn-info btn-success btn-warning btn-danger btn-inverse");   
             $('#code-btn-color').text(' btn-link');
     });
 
     //Button Size
-    $('#btn-mini').click(function(){
+    $('#btn-xs').click(function(){
         $(this).addClass('active').parent().siblings().find('a').removeClass('active');
-        $('#bt').addClass("btn-mini").removeClass("btn-small btn-large");
+        $('#bt').addClass("btn-xs").removeClass("btn-sm btn-lg");
         $('#stage p a').css({'margin-top': '10px'});
-        $('#code-btn-size').html(' btn-mini');
+        $('#code-btn-size').html(' btn-xs');
     });
 
-    $('#btn-small').click(function(){
+    $('#btn-sm').click(function(){
         $(this).addClass('active').parent().siblings().find('a').removeClass('active');
-        $('#bt').addClass("btn-small").removeClass("btn-mini btn-large"); 
+        $('#bt').addClass("btn-sm").removeClass("btn-xs btn-lg"); 
         $('#stage p a').css('margin-top','9px');    
-        $('#code-btn-size').html(' btn-small');  
+        $('#code-btn-size').html(' btn-sm');  
     });
 
     $('#default').click(function(){
     	$(this).addClass('active').parent().siblings().find('a').removeClass('active');
-        $('#bt').removeClass("btn-small btn-large btn-mini");
+        $('#bt').removeClass("btn-sm btn-lg btn-xs");
         $('#stage p a').css('margin-top','8px');
         $('#code-btn-size').html('');
     });
 
-    $('#btn-large').click(function(){
+    $('#btn-lg').click(function(){
         $(this).addClass('active').parent().siblings().find('a').removeClass('active');
-        $('#bt').addClass("btn-large").removeClass("btn-small btn-mini");    
+        $('#bt').addClass("btn-lg").removeClass("btn-sm btn-xs");    
         $('#stage p a').css({'margin-top': '0'});
-        $('#code-btn-size').html(' btn-large');
+        $('#code-btn-size').html(' btn-lg');
     });
 
     //btn-default
@@ -142,8 +142,8 @@ $('document').ready(function(){
     //icon-white
     $('#icon-white').click(function(){
         $(this).addClass('active').siblings().removeClass('active');
-        $('#bt i').addClass('icon-white');
-        $('#code-icon-color').html(' icon-white');
+        $('#bt i').addClass('icon-white'); // REBOVER ICON WHITE
+        $('#code-icon-color').html(' icon-white'); // REBOVER ICON WHITE
     });
 
     //icon-black
@@ -158,18 +158,18 @@ $('document').ready(function(){
     $('#icones').click(function(){
         $('#icon-remove').addClass('disabled');
         if(!$('#bt-submit').hasClass('active') && !$('#bt-input').hasClass('active')){
-            if($('#icones i').hasClass('icon-folder-open')){
-                $('#icones i').removeClass('icon-folder-open').addClass('icon-folder-close active');
-            }else if($('#icones i').hasClass('icon-folder-close')){
-                $('#icones i').removeClass('icon-folder-close').addClass('icon-folder-open');
+            if($('#icones i').hasClass('glyphicon glyphicon-folder-open')){
+                $('#icones i').removeClass('glyphicon glyphicon-folder-open').addClass('glyphicon glyphicon-folder-close active');
+            }else if($('#icones i').hasClass('glyphicon glyphicon-folder-close')){
+                $('#icones i').removeClass('glyphicon glyphicon-folder-close').addClass('glyphicon glyphicon-folder-open');
             }
             $("#galleryIcons").slideToggle("slow");
             $(this).toggleClass("active"); return false;
         }
     });
 
-    $('ul.the-icons li i').click(function(){
-       if($('#icon-white').hasClass('active')   &&  $('#code-icon-color').html('icon-white')){
+    $('ul.the-icons li span').click(function(){
+       if($('#icon-white').hasClass('active')   &&  $('#code-icon-color').html('icon-white')){ // REMOBER ICON-WHITE
         $('#code-icon-color').html('');
         $('#icon-white').removeClass('active');
        }
@@ -184,8 +184,8 @@ $('document').ready(function(){
             $('#bt #left-ico').prepend("<i></i>");
         }
         var $txtUser = $(this).attr('class');
-        if($('#bt i').hasClass('icon-white')){
-            $('#bt i').attr('class',''+$txtUser+' icon-white');
+        if($('#bt i').hasClass('icon-white')){ // REMOBER ICON-WHITE
+            $('#bt i').attr('class',''+$txtUser+' icon-white'); // REMOBER ICON-WHITE
         }else{
             $('#bt i').attr('class',''+$txtUser+'');
         } 
@@ -211,12 +211,11 @@ $('document').ready(function(){
         $('#code-icon-left, #code-icon-right').hide();
         if(!$('#icon-remove').hasClass('disabled')){
             $(this).addClass('disabled');
-            $('#icon-ok').attr('class','btn');
             $('#left-ico i, #right-ico i').attr('class','');
             $('#icon-ok, #icon-white, #icon-black, #icon-left, #icon-right').addClass('disabled');
             $('ul.the-icons li').removeClass('iconActive');
             $("#galleryIcons").slideToggle("slow");
-            $('#icones i').removeClass('icon-folder-open').addClass('icon-folder-close');  
+            $('#icones i').removeClass('glyphicon glyphicon-folder-open').addClass('glyphicon glyphicon-folder-close');  
         }
     }); 
 
@@ -281,7 +280,7 @@ $('document').ready(function(){
         $('ul.the-icons li').removeClass('iconActive');
         $("#galleryIcons").slideUp('slow');
         $('#icones').addClass('disabled');
-        $('#icones i').removeClass('icon-folder-open').addClass('icon-folder-close');
+        $('#icones i').removeClass('glyphicon glyphicon-folder-open').addClass('glyphicon glyphicon-folder-close');
         /*CONSERTO BUG*/
         var $escolhido = $('#right-ico').find('i');
         $('#left-ico').html($escolhido);
